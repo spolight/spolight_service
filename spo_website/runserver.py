c4df818c0f97e232252b00ccff3ca34387026795
hyunjaepark <wigglegigglegoggle@gmail.com>
2016-06-29 21:20:57 +0900

    define basic structure of website

---
+++ spo_website/runserver.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
+
+if __name__ == '__main__' :
+    print 'starting server...'
+    app.run(host='0.0.0.0', port=5000, debug=Tre()
